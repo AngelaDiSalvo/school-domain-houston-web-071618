e33116bc43ec7729dc3d71c37fe0f0bd2422d193
AngelaDiSalvo <angela.disalvo@gmail.com>
2018-07-18 22:31:07 -0500

    school domain lab mod1

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require "pry"
+
+class School
+  attr_accessor :roster, :name
+
+  def initialize(name)
+    @roster = {}
+    @name = name
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+
+    def grade(grade)
+      @roster[grade]
+    end
+
+    def sort
+      sorted = {}
+      @roster.map do |grade, names|
+        sorted[grade] = names.sort
+      end
+      sorted
+    end
+
+  end
+
+end
